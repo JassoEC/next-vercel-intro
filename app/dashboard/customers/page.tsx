b51248328d22796ee152837b9fd05b73d8f23191
Emanuel Campos <isw.ecamposj@gmail.com>
2024-11-11 20:52:07 -0600

    chapter-5

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,9 @@
+export default function CustomersPage() {
+  return (
+    <div>
+      <h1>
+        Customers Page
+      </h1>
+    </div>
+  )
+}
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,9 @@
+export default function InvoicesPage() {
+  return (
+    <div>
+      <h1>
+        Invoices Page
+      </h1>
+    </div>
+  )
+}
